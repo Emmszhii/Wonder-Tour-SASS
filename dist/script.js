517a09eee63fbad969eb707f2c5f9af9e051c2f6
Emmszhii <emmanuelmanalo707@gmail.com>
2023-03-23 15:02:20 +0800

    header

--- dist/script.js
+++ dist/script.js
@@ -2,12 +2,22 @@ const headerBottomToggle = document.querySelector(".header__bottom__toggle");
 const headerBottomNav = document.querySelector(".header__bottom");
 const headerContactToggle = document.querySelector(".header__contact__toggle");
 const headerContactContent = document.querySelector(".header__contact");
+const headerNavToggleIcon = document.querySelectorAll(".header__toggle__item");
 
 const onClickHeaderBottom = () => {
   headerBottomToggle.classList.toggle("open");
   headerBottomNav.classList.toggle("open");
   headerContactToggle.classList.remove("open");
   headerContactContent.classList.remove("open");
+
+  closeAllHeaderItem();
+};
+
+const closeAllHeaderItem = () => {
+  headerNavToggleIcon.forEach((item) => {
+    item.nextElementSibling.classList.remove("open");
+    item.classList.remove("active");
+  });
 };
 
 const onClickContactToggle = () => {
@@ -15,9 +25,23 @@ const onClickContactToggle = () => {
   headerContactContent.classList.toggle("open");
   headerBottomToggle.classList.remove("open");
   headerBottomNav.classList.remove("open");
+  closeAllHeaderItem();
+};
+
+const onClickVisibleToggle = (e) => {
+  const element = e.currentTarget;
+  element.classList.toggle("active");
+  element.querySelector("span i").classList.toggle("active");
+  element.nextElementSibling.classList.toggle("open");
 };
 
 if (headerBottomToggle)
   headerBottomToggle.addEventListener("click", onClickHeaderBottom);
 if (headerContactToggle)
   headerContactToggle.addEventListener("click", onClickContactToggle);
+
+if (headerNavToggleIcon) {
+  headerNavToggleIcon.forEach((item) => {
+    item.addEventListener("click", onClickVisibleToggle);
+  });
+}
