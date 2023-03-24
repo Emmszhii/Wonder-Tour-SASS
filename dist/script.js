aeb9dc1872b547dc7db78d8dcefd39018a40cd01
Emmszhii <emmanuelmanalo707@gmail.com>
2023-03-24 12:33:22 +0800

    banner

--- dist/script.js
+++ dist/script.js
@@ -35,11 +35,35 @@ const onClickContactToggle = () => {
 
 const onClickVisibleToggle = (e) => {
   const element = e.currentTarget.parentElement;
-  element.classList.toggle("active");
-  element.querySelector("i").classList.toggle("active");
   const content = element.querySelector(".content");
-  content.classList.toggle("open");
+  if (element.classList.contains("active")) {
+    element.classList.remove("active"); // parent
+    element.querySelector("i").classList.remove("active"); // icon
+    content.classList.remove("open"); // content
+  } else {
+    closeAllHeaderItem();
+    element.classList.toggle("active"); // parent
+    element.querySelector("i").classList.toggle("active"); // icon
+    content.classList.toggle("open"); // content
+  }
 };
+
+var slider = tns({
+  container: ".banner__wrapper",
+  axis: "vertical",
+  items: 1,
+  slideBy: "page",
+  controlsPosition: "bottom",
+  nav: true,
+  autoplay: true,
+  autoplayPosition: "bottom",
+  // autoplayHoverPause: true,
+  navPosition: "bottom",
+  autoplayButton: false,
+  // autoHeight: true,
+  loop: true,
+  // navContainer: ".banner__control",
+});
 
 if (headerBottomToggle)
   headerBottomToggle.addEventListener("click", onClickHeaderBottom);
